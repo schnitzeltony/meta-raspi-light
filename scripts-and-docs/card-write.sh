5cbd30bf61f1f048f8627595bb0c195ee86f366c
Andreas Müller <schnitzeltony@gmail.com>
2018-05-13 21:18:31 +0200

    card-write.sh: Change email/copyright year
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- scripts-and-docs/card-write.sh
+++ scripts-and-docs/card-write.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # write-card.sh
-# (c) Copyright 2013-2016 Andreas Müller <schnitzeltony@googlemail.com>
+# (c) Copyright 2013-2018 Andreas Müller <schnitzeltony@gmail.com>
 # Licensed under terms of GPLv2
 #
 # This script writes image to sdcard and aligns rootfs partition to max size.
